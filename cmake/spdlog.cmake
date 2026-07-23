cb04666f57546ad0ca8cc9a13c21f6fcf961c290
amy <ctrlalternativedelete@gmail.com>
2026-07-24 00:01:07 +1000

    fix: spdlog fixes for switch

--- cmake/spdlog.cmake
+++ cmake/spdlog.cmake
@@ -7,3 +7,15 @@ FetchContent_Declare(
 )
 
 FetchContent_MakeAvailable(spdlog)
+
+if(NINTENDO_SWITCH)
+        if(TARGET spdlog)
+                target_compile_definitions(spdlog
+                                PUBLIC _POSIX_C_SOURCE=200809L _DEFAULT_SOURCE)
+        endif()
+
+        if(TARGET spdlog_header_only)
+                target_compile_definitions(spdlog_header_only
+                                INTERFACE _POSIX_C_SOURCE=200809L _DEFAULT_SOURCE)
+        endif()
+endif()
