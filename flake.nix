2e3a5c369386a0fcd1a14ee8146c99562240fd18
amy <ctrlalternativedelete@gmail.com>
2026-07-24 19:13:15 +1000

    fix: flake for pkgconfig

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    devkitNix.url = "github:bandithedoge/devkitNix";
+    devkitNix.url = "github:amydevs/devkitNix";
   };
 
   outputs =
@@ -22,17 +22,7 @@
         };
       in
       {
-        devShells.default = pkgs.mkShell.override { stdenv = pkgs.devkitNix.stdenvA64; } {
-          buildInputs = [
-            pkgs.pkg-config
-          ];
-          shellHook = ''
-            export SYSROOT="${pkgs.devkitNix.devkitA64}"
-            export PKG_CONFIG="${pkgs.pkg-config}/bin/pkg-config"
-            export PKG_CONFIG_LIBDIR="$DEVKITPRO/portlibs/switch/lib/pkgconfig"
-            export PKG_CONFIG_SYSROOT_DIR="$SYSROOT"
-          '';
-        };
+        devShells.default = pkgs.mkShell.override { stdenv = pkgs.devkitNix.stdenvA64; } {};
         packages.default = pkgs.devkitNix.stdenvA64.mkDerivation {
           name = "devkitA64-example";
           src = ./.;
